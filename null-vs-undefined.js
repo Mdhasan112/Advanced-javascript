8dfa3d4dd3cb231d7769713d485fbcab541c8673
hasan <mdhasanhk.98@gmail.com>
2022-07-17 14:54:24 -0400

    null-vs-undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,14 @@
+let friend;
+// function add(num1, num2) {
+//     console.log(num1 + num2);
+//     return
+// }
+function add(num1, num2) {
+    console.log(num1, num2);
+    
+}
+let result = add(20, 10);
+console.log(result);
+
+const student = {name: "liton", number: 254554}
+console.log(student.gf)
