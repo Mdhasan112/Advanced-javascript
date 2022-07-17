aa976b1812ae8d51df4c5338ec0c4959cc73571b
hasan <mdhasanhk.98@gmail.com>
2022-07-17 14:31:31 -0400

    true-false

---
+++ true-false.js
@@ -0,0 +1,15 @@
+// const age = 0;
+// if(age) {
+//     console.log("This is true")
+// }
+// else{
+//     console.log("This is false")
+// }
+
+const names = "";
+if(names.length > 0) {
+    console.log("This is true")
+}
+else{
+    console.log("This is false")
+}
