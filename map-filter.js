aa60d1f310e83a7cbe3a882c757c7277352776ce
hasan <mdhasanhk.98@gmail.com>
2022-07-24 04:30:41 -0400

    scope

--- map-filter.js
+++ map-filter.js
@@ -1,9 +1,17 @@
 const numbers = [8, 7, 10, 15, 5]
 const output = [];
 
-for(let i = 0; i < numbers.length; i++) {
-    const element = numbers[i];
-    const result = element * element;
-    output.push(result);
+// for(let i = 0; i < numbers.length; i++) {
+//     const element = numbers[i];
+//     const result = element * element;
+//     output.push(result);
+// }
+
+function square (element) {
+    return element * element;
 }
-console.log(output);
+
+numbers.map(function(element) {
+    console.log(element, index, array)
+})
+// console.log(element);
---
+++ scope.js
@@ -0,0 +1,15 @@
+let bonus = 20;
+function sum (first, second) {
+    let result = first + second + bonus;
+    if(result > 9) {
+        var mood = "fruits"
+        mood = "man"
+        console.log(mood)
+    }
+    console.log(mood);
+    return result;
+}
+
+
+const total = sum(3, 7);
+// console.log(total);
